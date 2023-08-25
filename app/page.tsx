ef6eeb92f6d889dba2e21e5af476e07a982c69c5
Chico Dias <chico@agenciavoolu.com>
2023-08-25 20:45:31 -0300

    inicial arquivos

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import { Carousel } from 'components/carousel';
+import { ThreeItemGrid } from 'components/grid/three-items';
+import Footer from 'components/layout/footer';
+import { Suspense } from 'react';
+
+export const runtime = 'edge';
+
+export const metadata = {
+  description: 'E-commerce de alta performace construido na Agência Voolu com Next.js, Vercel, e Shopify.',
+  openGraph: {
+    type: 'website'
+  }
+};
+
+export default async function HomePage() {
+  return (
+    <>
+      <ThreeItemGrid />
+      <Suspense>
+        <Carousel />
+        <Suspense>
+          <Footer />
+        </Suspense>
+      </Suspense>
+    </>
+  );
+}
